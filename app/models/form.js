aff43d4c6ef4e911d371c72027670b0f8b545426
Michael Daines <mdaines@fastmail.com>
2015-12-30 15:16:15 -0500

    Move blankEntry into form model.

--- app/models/form.js
+++ app/models/form.js
@@ -1,8 +1,30 @@
 import DS from 'ember-data';
 
+function blankEntry(blocks) {
+  var entry = Ember.Object.create();
+
+  for (let i = 0; i < blocks.length; i++) {
+    let block = blocks[i];
+
+    if (block.type === "section") {
+      if (block.repeat) {
+        entry.set(block.id, [blankEntry(block.children)]);
+      } else {
+        entry.set(block.id, blankEntry(block.children));
+      }
+    }
+  }
+
+  return entry;
+}
+
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
   blocks: DS.attr(),
-  templates: DS.attr()
+  templates: DS.attr(),
+  
+  blankEntry: function() {
+    return blankEntry(this.get("blocks"));
+  }
 });
--- app/routes/form.js
+++ app/routes/form.js
@@ -1,23 +1,5 @@
 import Ember from 'ember';
 
-function blankEntry(blocks) {
-  var entry = Ember.Object.create();
-  
-  for (let i = 0; i < blocks.length; i++) {
-    let block = blocks[i];
-    
-    if (block.type === "section") {
-      if (block.repeat) {
-        entry.set(block.id, [blankEntry(block.children)]);
-      } else {
-        entry.set(block.id, blankEntry(block.children));
-      }
-    }
-  }
-  
-  return entry;
-}
-
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('form', params.form_id);
@@ -26,6 +8,6 @@ export default Ember.Route.extend({
   setupController(controller, model) {
     this._super(controller, model);
     
-    controller.set("entry", blankEntry(model.get("blocks")));
+    controller.set("entry", model.blankEntry());
   }
 });
