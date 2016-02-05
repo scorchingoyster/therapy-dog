85903795aaef2af2a9af96c022f64e190fc7002d
Mike Daines <daines@email.unc.edu>
2016-02-05 14:29:18 -0500

    Replace forms mock with proxy.

---
+++ server/proxies/api.js
@@ -0,0 +1,17 @@
+/*jshint node:true*/
+var proxyPath = '/api';
+
+module.exports = function(app) {
+  // For options, see:
+  // https://github.com/nodejitsu/node-http-proxy
+  var proxy = require('http-proxy').createProxyServer({});
+
+  proxy.on('error', function(err, req) {
+    console.error(err, req.url);
+  });
+
+  app.use(proxyPath, function(req, res, next){
+    // include root path in proxied request
+    proxy.web(req, res, { target: 'http://127.0.0.1:3000/' });
+  });
+};
