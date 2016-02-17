e3843c01759d3d07f52c24a50043cb3cd90fdd18
Mike Daines <daines@email.unc.edu>
2016-02-17 15:39:44 -0500

    Add styles for error messages and invalid text blocks.

--- app/components/block-text.js
+++ app/components/block-text.js
@@ -3,8 +3,11 @@ import FocusEntryAction from 'therapy-dog/mixins/focus-entry-action';
 
 export default Ember.Component.extend(FocusEntryAction, {
   classNames: ['block', 'text'],
-  classNameBindings: ['required'],
+  classNameBindings: ['required', 'invalid'],
   required: Ember.computed.alias('entry.block.required'),
+  invalid: Ember.computed('entry.errors', 'entry.attempted', function() {
+    return !Ember.isEmpty(this.get('entry.errors')) && this.get('entry.attempted');
+  }),
   
   focusOut: function() {
     this.set('entry.attempted', true);
