92cca91dc4ee1d1fe0bd57f79800a77e9fcbe79b
Mike Daines <daines@email.unc.edu>
2016-02-17 16:35:39 -0500

    Flesh out file block from prototype.

--- app/components/block-file.js
+++ app/components/block-file.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import FocusEntryAction from 'therapy-dog/mixins/focus-entry-action';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(FocusEntryAction, {
   classNames: ['block', 'file'],
   classNameBindings: ['required', 'invalid'],
   required: Ember.computed.alias('entry.block.required'),
@@ -8,33 +9,99 @@ export default Ember.Component.extend({
     return !Ember.isEmpty(this.get('entry.errors')) && this.get('entry.attempted');
   }),
   
+  focusOut: function() {
+    this.set('entry.attempted', true);
+  },
+  
+  state: 'empty',
+  isEmpty: Ember.computed.equal('state', 'empty'),
+  isProgress: Ember.computed.equal('state', 'progress'),
+  isError: Ember.computed.equal('state', 'error'),
+  isComplete: Ember.computed.equal('state', 'complete'),
+  
+  upload() {
+    var formData = new FormData();
+    formData.append('file', this.get('file'), this.get('file.name'));
+    
+    this.xhr = new XMLHttpRequest();
+    this.xhr.responseType = 'json';
+    
+    this.xhr.upload.onprogress = (e) => {
+      this.send('progress', e.loaded, e.total);
+    };
+    
+    this.xhr.upload.onerror = () => {
+      this.send('error');
+    };
+    
+    this.xhr.onload = () => {
+      if (this.xhr.status === 200) {
+        this.send('complete', this.xhr.response);
+      } else {
+        this.send('error');
+      }
+    };
+    
+    this.xhr.open('POST', '/api/uploads', true);
+    this.xhr.send(formData);
+    
+    this.set('state', 'progress');
+    this.set('progress', Ember.Object.create({ total: this.get('file.size'), loaded: 0, ratio: 0 }));
+  },
+  
   actions: {
-    select(files) {
+    choose(files) {
       if (files.length > 0) {
-        var formData = new FormData();
+        let file = files[0];
+        this.set('file', file);
         
-        var file = files[0];
-        formData.append('file', file, file.name);
-        
-        var xhr = new XMLHttpRequest();
-        xhr.responseType = 'json';
-        
-        xhr.onload = () => {
-          if (xhr.status === 200) {
-            this.set('entry.value', xhr.response);
-            this.set('progress', null);
-          }
-        };
-        
-        xhr.open('POST', '/api/uploads', true);
-        
-        xhr.send(formData);
+        this.upload();
       }
     },
     
-    clear() {
-      this.set('entry.value', null);
+    cancel() {
+      this.xhr.abort();
+      
+      this.set('state', 'empty');
       this.set('progress', null);
+      this.set('file', null);
+    },
+    
+    progress(loaded, total) {
+      let ratio;
+      
+      if (total > 0) {
+        ratio = loaded / total;
+      } else {
+        ratio = 0;
+      }
+      
+      this.set('progress', { loaded, total, ratio });
+    },
+    
+    error() {
+      this.set('state', 'error');
+    },
+    
+    retry() {
+      this.upload();
+    },
+    
+    complete(response) {
+      this.set('state', 'complete');
+      this.set('entry.value', response);
+      this.set('progress', null);
+    },
+    
+    remove() {
+      this.set('state', 'empty');
+      this.set('entry.value', null);
+      this.set('file', null);
+      this.set('progress', null);
+    },
+
+    focusEntry() {
+      this.$('input').focus();
     }
   }
 });
---
+++ app/components/progress-bar.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['progress-bar'],
+  
+  didRender() {
+    this._super(...arguments);
+    this.$().css('width', (this.get('ratio') * 100) + '%');
+  }
+});
