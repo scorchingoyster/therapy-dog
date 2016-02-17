11ea99ac20274c4509252166eafae2877b170cbe
Mike Daines <daines@email.unc.edu>
2016-02-17 15:28:07 -0500

    Remove extra promise wrapping in deposit route.

--- app/routes/deposit.js
+++ app/routes/deposit.js
@@ -3,16 +3,12 @@ import ObjectEntry from 'therapy-dog/utils/object-entry';
 
 export default Ember.Route.extend({
   model(params) {
-    let promise = this.store.findRecord('form', params.form_id).then(function(form) {
+    return this.store.findRecord('form', params.form_id).then(function(form) {
       return Ember.Object.create({
         form: form,
         entry: ObjectEntry.create({ block: form })
       });
     });
-    
-    let proxy = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin).create({ promise });
-    
-    return proxy;
   },
   
   afterModel() {
