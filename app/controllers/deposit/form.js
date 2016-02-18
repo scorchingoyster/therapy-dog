e8d1278c1a6d34061cb9f24a2fba7d05762502b0
Mike Daines <daines@email.unc.edu>
2016-02-18 15:13:22 -0500

    Add a "submit deposit" button.

--- app/controllers/deposit/form.js
+++ app/controllers/deposit/form.js
@@ -4,6 +4,26 @@ import ObjectEntry from 'therapy-dog/utils/object-entry';
 export default Ember.Controller.extend({
   entryEvents: Ember.inject.service(),
   
+  validate() {
+    let model = this.get('model');
+    let firstBadEntry;
+    
+    model.forEach(function(entry) {
+      entry.set("attempted", true);
+      
+      if (entry.get("errors.length") > 0 && !firstBadEntry) {
+        firstBadEntry = entry;
+      }
+    });
+    
+    if (firstBadEntry) {
+      this.get("entryEvents").trigger("focus", firstBadEntry);
+      return false;
+    } else {
+      return true;
+    }
+  },
+  
   actions: {
     reset() {
       this.set('model', ObjectEntry.create({ block: this.get('model.block') }));
@@ -13,21 +33,12 @@ export default Ember.Controller.extend({
       console.log(JSON.stringify(this.get('model').flatten()));
     },
     
+    deposit() {
+      return this.validate();
+    },
+    
     validate() {
-      let model = this.get('model');
-      let firstBadEntry;
-      
-      model.forEach(function(entry) {
-        entry.set("attempted", true);
-        
-        if (entry.get("errors.length") > 0 && !firstBadEntry) {
-          firstBadEntry = entry;
-        }
-      });
-      
-      if (firstBadEntry) {
-        this.get("entryEvents").trigger("focus", firstBadEntry);
-      }
+      this.validate();
     }
   }
 });
