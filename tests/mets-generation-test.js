c64dc298d307b661f2d355db9874fe330cbc1172
Mike Daines <daines@email.unc.edu>
2016-02-22 14:43:18 -0500

    Add failing test for generating admin metadata sections.

--- tests/mets-generation-test.js
+++ tests/mets-generation-test.js
@@ -4,19 +4,23 @@ var generateMets = require("../lib/generate-mets");
 var generateBundle = require("../lib/generate-bundle");
 
 describe("METS generation", function() {
-  
   describe("with metadata and files", function() {
     var form = {
       blocks: [
         { type: "file", key: "thesis" },
         { type: "text", key: "title" }
       ],
-      bundle: "item kind='File' label=title { file { thesis; } metadata { partial 'thesis'; } }",
+      bundle: "item kind='File' label=title { file { thesis; } metadata { partial 'thesis'; } metadata { partial 'unpublished'; } }",
       templates: [
         {
           id: "thesis",
           type: "xml",
           template: "element 'mods' xmlns='http://www.loc.gov/mods/v3' @compact=true { title -> (element 'titleInfo') (element 'title'); }"
+        },
+        {
+          id: "unpublished",
+          type: "xml",
+          template: "element 'accessControl' xmlns='http://cdr.unc.edu/definitions/acl' published='false' {}"
         }
       ]
     };
@@ -77,6 +81,22 @@ describe("METS generation", function() {
       assert.equal(title.text(), "My Thesis");
     });
   
+    it("should generate an amdSec element", function() {
+      var amdSec = doc.get("/mets:mets/mets:amdSec", { mets: "http://www.loc.gov/METS/" });
+      assert.ok(amdSec);
+      
+      var rightsMD = amdSec.get("mets:rightsMD", { mets: "http://www.loc.gov/METS/" });
+      assert.equal(rightsMD.attr("ID").value(), bundle.children[0].children[2].id);
+    
+      var mdWrap = rightsMD.get("mets:mdWrap", { mets: "http://www.loc.gov/METS/" });
+      assert.ok(mdWrap);
+      assert.equal(mdWrap.attr("MDTYPE").value(), "OTHER");
+    
+      var accessControl = mdWrap.get("mets:xmlData/acl:accessControl", { mets: "http://www.loc.gov/METS/", acl: "http://cdr.unc.edu/definitions/acl" });
+      assert.ok(accessControl);
+      assert.equal(accessControl.attr("published").value(), "false");
+    });
+  
     it("should generate a file element", function() {
       var file = doc.get("/mets:mets/mets:fileSec/mets:fileGrp/mets:file", { mets: "http://www.loc.gov/METS/" });
       assert.ok(file);
@@ -102,6 +122,13 @@ describe("METS generation", function() {
       var fptr = div.get("mets:fptr", { mets: "http://www.loc.gov/METS/" });
       assert.ok(fptr);
       assert.equal(fptr.attr("FILEID").value(), bundle.children[0].children[0].id);
+    });
+  
+    it("should generate sections in order", function() {
+      var sections = doc.find("/mets:mets/*", { mets: "http://www.loc.gov/METS/" });
+      var names = sections.map(function(s) { return s.name(); });
+      
+      assert.deepEqual(["metsHdr", "amdSec", "dmdSec", "fileSec", "structMap"], names);
     });
   
   });
