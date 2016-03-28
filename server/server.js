07ea8b32596e7f8de486e1d665a4a08f6a50f8fd
Mike Daines <daines@email.unc.edu>
2016-03-28 16:11:19 -0400

    API served at / rather than /api.

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const router = require('./router');
 
 let app = express();
 app.use(morgan('dev'));
-app.use('/api', router);
+app.use('/', router);
 
 app.listen(process.env.PORT, process.env.HOST, function() {
   console.log('Server started on %s:%d', process.env.HOST, process.env.PORT);
