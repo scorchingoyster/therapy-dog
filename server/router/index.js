b5584b85984a80f9dfc6e3b04207fcfd88973b87
Mike Daines <daines@email.unc.edu>
2016-03-28 16:05:12 -0400

    Move router into router/index.js

--- server/router/index.js
+++ server/router/index.js
@@ -6,10 +6,10 @@ var upload = multer({ dest: process.env.UPLOADS_DIRECTORY });
 
 router.use(require('body-parser').json());
 
-router.get('/forms', require('./handlers/forms').index);
-router.get('/forms/:id', require('./handlers/forms').show);
-router.post('/deposits', require('./handlers/deposits').create);
-router.post('/uploads', upload.single('file'), require('./handlers/uploads').create);
+router.get('/forms', require('../handlers/forms').index);
+router.get('/forms/:id', require('../handlers/forms').show);
+router.post('/deposits', require('../handlers/deposits').create);
+router.post('/uploads', upload.single('file'), require('../handlers/uploads').create);
 
 router.use(function(err, req, res, next) {
   res.status(500);
--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ require('source-map-support').install();
 
 var express = require('express');
 var morgan = require('morgan');
-var api = require('./api');
+var router = require('./router');
 
 var app = express();
 app.use(morgan('dev'));
-app.use('/api', api);
+app.use('/api', router);
 
 app.listen(process.env.PORT, process.env.HOST, function() {
   console.log('Server started on %s:%d', process.env.HOST, process.env.PORT);
