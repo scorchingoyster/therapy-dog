7c645d4c0323dc76204979d99bb25664e990c80a
Mike Daines <daines@email.unc.edu>
2016-03-29 17:06:15 -0400

    Explicit SWORD errors.
    - SWORD errors dumped to error log
    - SWORD error responses are 500 errors rather than { status: 'ERROR' }

--- server/lib/errors.js
+++ server/lib/errors.js
@@ -25,3 +25,11 @@ exports.UploadNotFoundError = function(message, extra) {
   this.extra = extra;
 };
 inherits(exports.UploadNotFoundError, Error);
+
+exports.SwordError = function(message, extra) {
+  Error.captureStackTrace(this, this.constructor);
+  this.name = this.constructor.name;
+  this.message = message;
+  this.extra = extra;
+}
+inherits(exports.SwordError, Error);
