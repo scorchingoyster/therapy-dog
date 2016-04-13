f0371c7d093a6bf3899a2106c8ba066b909d3f72
Mike Daines <daines@email.unc.edu>
2016-04-13 12:44:14 -0400

    Set name for inputs in radio blocks.

--- client/app/components/radio-button.js
+++ client/app/components/radio-button.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'input',
   type: 'radio',
-  attributeBindings: ['checked', 'type', 'value'],
+  attributeBindings: ['checked', 'type', 'value', 'name'],
 
   checked: Ember.computed('value', 'groupValue', function() {
     return this.get('groupValue') === this.get('value');
