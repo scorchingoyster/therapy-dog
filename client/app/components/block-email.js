0823f7534f13f1c82e5ea2ce792fac4098f65b41
lfarrell <lfarrell@email.unc.edu>
2016-04-22 10:21:19 -0400

    Add email block type

---
+++ client/app/components/block-email.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+import FocusEntryAction from 'therapy-dog/mixins/focus-entry-action';
+
+export default Ember.Component.extend(FocusEntryAction, {
+  classNames: ['block', 'email'],
+  classNameBindings: ['required', 'invalid'],
+  required: Ember.computed.alias('entry.required'),
+  invalid: Ember.computed.alias('entry.invalid'),
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    
+    if (Ember.isBlank(this.get('entry.value'))) {
+      this.set('entry.value', this.get('entry.block.defaultValue') || '');
+    }
+  },
+  
+  focusOut: function() {
+    this.set('entry.attempted', true);
+  },
+  
+  actions: {
+    focusEntry: function() {
+      this.$('input').focus();
+    }
+  }
+});
