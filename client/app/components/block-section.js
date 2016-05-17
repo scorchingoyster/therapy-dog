7d2c82a30b4c5aac1822f88dcef70ffefbd22c04
Mike Daines <daines@email.unc.edu>
2016-05-17 14:58:43 -0400

    Adjust focus for repeating sections.
    - After adding an item, focus its first ValueEntry if present.
    - Set focus after removing an item.

--- client/app/components/block-section.js
+++ client/app/components/block-section.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import ObjectEntry from 'therapy-dog/utils/object-entry';
 
 export default Ember.Component.extend({
+  entryEvents: Ember.inject.service(),
+  
   classNames: ['block', 'section'],
   classNameBindings: ['repeat'],
   repeat: Ember.computed.alias('entry.block.repeat'),
@@ -25,13 +27,38 @@ export default Ember.Component.extend({
   actions: {
     add() {
       if (this.get('entry.block.repeat')) {
-        this.get('entry.value').pushObject(this.createBlankEntry());
+        let entry = this.createBlankEntry();
+        this.get('entry.value').pushObject(entry);
+        
+        Ember.run.next(this, function() {
+          this.get('entryEvents').trigger('focus', entry);
+        });
       }
     },
     
     remove(entry) {
       if (this.get('entry.block.repeat')) {
-        this.get('entry.value').removeObject(entry);
+        // Look for an entry to focus after removing this one.
+        let entries = this.get('entry.value');
+        let index = entries.indexOf(entry);
+        
+        // If there is an entry after the one we're removing, focus that.
+        // Otherwise, if there is one before, focus that.
+        let focusEntry = entries.get(index + 1) || entries.get(index - 1);
+        
+        entries.removeObject(entry);
+        
+        // If we found an entry to focus, do that.
+        // Otherwise, focus the "Add" button.
+        if (focusEntry) {
+          Ember.run.next(this, function() {
+            this.get('entryEvents').trigger('focus', focusEntry);
+          });
+        } else {
+          Ember.run.next(this, function() {
+            this.$('.add button').focus();
+          });
+        }
       }
     }
   }
