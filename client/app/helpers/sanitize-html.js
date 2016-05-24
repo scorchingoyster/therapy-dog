e64006f37aa61535c75153ef041cc57940c2f182
Mike Daines <daines@email.unc.edu>
2016-05-24 14:09:44 -0400

    Add target="_blank" to links in sanitized HTML.

--- client/app/helpers/sanitize-html.js
+++ client/app/helpers/sanitize-html.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 /* globals DOMPurify */
 
+// Add target="_blank" to links.
+DOMPurify.addHook('afterSanitizeAttributes', function(node) {
+  if ('target' in node) {
+    node.setAttribute('target', '_blank');
+  }
+});
+
 export default Ember.Helper.helper(function(params) {
   return Ember.String.htmlSafe(DOMPurify.sanitize(params[0]));
 });
