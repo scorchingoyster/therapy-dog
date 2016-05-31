aa9f620002f614ec20ad5d4a1feec238da6f0da7
Mike Daines <daines@email.unc.edu>
2016-05-31 12:31:37 -0400

    Add basic CSS for input summaries.

--- server/lib/mailer.js
+++ server/lib/mailer.js
@@ -20,6 +20,7 @@ const transport = nodemailer.createTransport(transportOptions);
 
 Handlebars.registerPartial('items-list-html', fs.readFileSync(path.join(__dirname, `../templates/-items-list-html.hbs`)).toString());
 Handlebars.registerPartial('items-list-text', fs.readFileSync(path.join(__dirname, `../templates/-items-list-text.hbs`)).toString());
+Handlebars.registerPartial('css', fs.readFileSync(path.join(__dirname, `../templates/-css.hbs`)).toString());
 
 function getTemplate(name, part) {
   return Handlebars.compile(fs.readFileSync(path.join(__dirname, `../templates/${name}.${part}.hbs`)).toString());
