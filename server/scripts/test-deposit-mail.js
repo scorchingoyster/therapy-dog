e03e8ec3e7aef05fdf333d830eda103effded813
Mike Daines <daines@email.unc.edu>
2016-06-21 16:56:11 -0400

    Update mailer testing script to include just an upload's id. Change variable names to be clearer.

--- server/scripts/test-deposit-mail.js
+++ server/scripts/test-deposit-mail.js
@@ -8,24 +8,24 @@ const mailer = require('../lib/mailer');
 const Form = require('../lib/models/form');
 const createTestUpload = require('../test/test-helpers').createTestUpload;
 
-let form = Form.findById('article');
+let getForm = Form.findById('article');
 
-let upload = createTestUpload('article.pdf', 'application/pdf', new Buffer('lorem ipsum'));
+let getUpload = createTestUpload('article.pdf', 'application/pdf', new Buffer('lorem ipsum'));
 
-let input = Promise.all([
-  upload,
+let getInput = Promise.all([
+  getUpload,
   readFile(path.join(__dirname, '../data/input/article.json'), 'utf8').then(JSON.parse)
 ])
-.spread((u, i) => {
-  i.article = u;
-  return i;
+.spread((upload, input) => {
+  input.article = upload.id;
+  return input;
 });
 
-let summary = Promise.all([form, input]).spread((f, i) => f.summarizeInput(i));
+let getSummary = Promise.all([getForm, getInput]).spread((form, input) => form.summarizeInput(input));
 
 Promise.all([
-  Promise.join(form, summary, Promise.resolve('depositor@example.com'), mailer.sendDepositReceipt),
-  Promise.join(form, summary, Promise.resolve('someone@example.com'), mailer.sendDepositNotification)
+  Promise.join(getForm, getSummary, Promise.resolve('depositor@example.com'), mailer.sendDepositReceipt),
+  Promise.join(getForm, getSummary, Promise.resolve('someone@example.com'), mailer.sendDepositNotification)
 ])
 .then(function(result) {
   console.log(result);
